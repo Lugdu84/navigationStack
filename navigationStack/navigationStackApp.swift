f76cd216e05fb07c15c1af37fbb3c63aa16aac69
David Grammatico <david.lugdunum@gmail.com>
2023-08-10 14:53:16 +0200

    Initial Commit

---
+++ navigationStack/navigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationStackApp.swift
+//  navigationStack
+//
+//  Created by David Grammatico on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct navigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
